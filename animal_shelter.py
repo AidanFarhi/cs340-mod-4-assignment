de3986dd50aaa249a2c7a33376f86f9ed34b0de2
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-06-08 19:22:16 -0400

    updated project-one

--- animal_shelter.py
+++ animal_shelter.py
@@ -30,6 +30,12 @@ class AnimalShelter:
         self.database = self.client[f"{db}"]
         self.collection = self.database[f"{collection}"]
 
+    def _validate_args(self, args: dict) -> bool:
+        """
+        Validates whether the arguments passed in are valid or not.
+        """
+        return args is None or not isinstance(args, dict) or len(args) == 0
+
     def create(self, data: dict) -> bool:
         """
         Creates an object in the database.
@@ -55,7 +61,7 @@ class AnimalShelter:
         Runs a query against the database and returns a list of results.
         """
         # check if the query is a non-null and non-empty dictionary
-        if query is None or not isinstance(query, dict) or len(query) == 0:
+        if self._validate_args(query):
             # if the above validation is true, return an empty list
             return []
         try:
@@ -63,5 +69,34 @@ class AnimalShelter:
             return self.collection.find(query).to_list()
         except Exception as e:
             # if the operation was not succesfull, raise an exception
+            print("error while searching the database")
+            raise e
+
+    def update(self, query: dict, update: dict) -> int:
+        """
+        Queries the database, updates found records, and returns number of records affected.
+        """
+        # check if the query and update args are valid
+        if self._validate_args(query) and self._validate_args(update):
+            return 0
+        try:
+            # use the update_many method to update the records and return the number affected
+            return self.collection.update_many(query, update).modified_count
+        except Exception as e:
+            # if the operation was not succesfull, raise an exception
+            print("error while trying to update")
+            raise e
+
+    def delete(self, query: dict) -> int:
+        """
+        Queries the database, deleted found records, and returns number of records affected.
+        """
+        # check if the query args is valid
+        if self._validate_args(query):
+            return 0
+        try:
+            # use the delete_many method to update the records and return the number affected
+            return self.collection.delete_many(query).deleted_count
+        except Exception as e:
             print("error while searching the database")
             raise e
